d472683c0aa732fd0ce1d7068a6a914e0ed01e23
armStrong759 <3058587756@qq.com>
2021-03-29 23:21:50 +0800

    两数之和

--- LeetCode/src/main/java/com/example/leetcode/ReverseTwoBits.java
+++ LeetCode/src/main/java/com/example/leetcode/ReverseTwoBits.java
@@ -3,14 +3,12 @@ package com.example.leetcode;
 /**
  * @data on 3/29/21 11:08 PM
  * @auther KC
- * @describe
+ * @describe 颠倒二进制位
  */
 public class ReverseTwoBits {
 
     /**
      * 颠倒给定的 32 位无符号整数的二进制位。
-     *
-     * 提示：
      *
      * 输入：00000010100101000001111010011100
      * 输出：00111001011110000010100101000000
---
+++ LeetCode/src/main/java/com/example/leetcode/TwoSum.java
@@ -0,0 +1,27 @@
+package com.example.leetcode;
+
+/**
+ * @data on 3/29/21 11:20 PM
+ * @auther KC
+ * @describe 两数之和
+ */
+public class TwoSum {
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * @param nums
+     * @param target
+     * @return
+     * 链接：https://leetcode-cn.com/problems/two-sum/
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        for(int i = 0;i<len;++i){
+            for(int j = i+1;j<len;++j){
+                if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
