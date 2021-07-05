3c665073727c802da0d9483a6e5bdf0f8ed4fac4
armStrong759 <3058587756@qq.com>
2021-07-05 10:12:29 +0800

    二叉树打印相关

---
+++ LeetCode/src/main/java/com/example/leetcode/tree/LevelOrder.java
@@ -0,0 +1,80 @@
+package com.example.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @data on 2021/7/5 9:38 上午
+ * @auther
+ * @describe  二叉树打印
+ */
+public class LevelOrder {
+
+    /**
+     * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+     * @param root
+     * @return 一维数组
+     */
+    public int[] levelOrder(TreeNode root) {
+        if(root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if(node.left != null) queue.add(node.left);
+            if(node.right != null) queue.add(node.right);
+        }
+        int[] res = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++)
+            res[i] = ans.get(i);
+        return res;
+    }
+    /**
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+     * @param root
+     * @return 二维数组
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    /**
+     * 从上到下按"之"字形打印二叉树，奇数层从左到右打印，偶数层从右到左打印，每一层打印到一行。
+     * @param root
+     * @return 二维数组
+     */
+    public List<List<Integer>> levelOrder3(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if (res.size() % 2 == 0) tmp.addLast(node.val); // 偶数层 -> 队列头部
+                else tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+}
---
+++ LeetCode/src/main/java/com/example/leetcode/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.example.leetcode.tree;
+
+/**
+ * @data on 2021/7/5 9:40 上午
+ * @auther
+ * @describe 二叉树基本结构
+ */
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
